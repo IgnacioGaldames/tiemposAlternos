9b53f78a7de7f434b575dd7308302426d4eff8f6
Ignacio Galdames <ignacio.g@desafiolatam.com>
2020-01-30 14:14:58 -0300

    blur en header según indice sueño

--- assets/js/blur.js
+++ assets/js/blur.js
@@ -1,17 +1,44 @@
 $(document).on(':passageend', function (ev) {
-    //state.variables["versionjuego"] = $miVersionJuego;
-    var indiceSueno = State.variables.playerIndiceSueno / 10;
-    var indiceBlur = 10 - indiceSueno;
-    var cssBlur = '0px 0px ' + indiceBlur + 'px rgba(255,255,255,1)';
+    (function () {
+        var cloudflare, letteringJs, lsLockLettering;
 
-    $(".lettering").css({
-        'color': 'transparent',
-        'text-shadow': cssBlur
-    });
+        lsLockLettering = LoadScreen.lock();
 
-    if (env == 'staging') {
-        console.log(indiceSueno);
-        console.log(indiceBlur);
-        console.log(cssBlur);
-    }
+        cloudflare = "https://cdnjs.cloudflare.com/ajax/libs/";
+
+        letteringJs = [cloudflare + "lettering.js/0.7.0/jquery.lettering.min.js"];
+
+        importScripts(letteringJs).then(function () {
+            LoadScreen.unlock(lsLockLettering);
+            $(".lettering").lettering();
+            //state.variables["versionjuego"] = $miVersionJuego;
+            var indiceSueno = State.variables.playerIndiceSueno / 10;
+            var indiceBlur = 10 - indiceSueno;
+            var cssBlur = '0px 0px ' + indiceBlur + 'px rgba(255,255,255,1)';
+            var cssBlur2 = '0px 0px ' + (indiceBlur / 2) + 'px rgba(255,255,255,1)';
+            var text = $(".lettering"),
+                // assuming lettering() has already been called on it
+                numLetters = text.find("span").length; // how many letters?
+
+            $(".lettering span").css({
+                'color': 'transparent',
+                'text-shadow': cssBlur
+            });
+
+            if (env == 'staging') {
+                console.log(numLetters);
+                console.log(indiceSueno);
+                console.log(indiceBlur);
+                console.log(cssBlur);
+            }
+            if (env === 'staging') {
+                console.log('letteringJs cargado');
+            }
+        })["catch"](function (err) {
+            console.log(err);
+        });
+
+    }).call(this);
+
+
 });
