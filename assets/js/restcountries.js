bc7f5d6b70558072c55137ed7ebe77cd60af734a
Ignacio Galdames <ignacio.g@desafiolatam.com>
2020-02-05 17:31:36 -0300

    añadir cantidad de muertos segun país

--- assets/js/restcountries.js
+++ assets/js/restcountries.js
@@ -1,20 +1,31 @@
 jQuery(document).ready(function ($) {
-    $.get('//restcountries.eu/rest/v2/all', function (data) {
+    $.get('https://restcountries.eu/rest/v2/all?fields=name;translations', function (data) {
         $.each(data, function (index, val) {
             var name = val.name;
             var es = val.translations.es;
             if (val.translations.es == null) {
                 es = name;
             }
-            var capital = val.capital;
-            var timezones = val.timezones;
-            var population = val.population;
-            var html = '<option>' + es + '</option>';
+            var html = '<option data-pais=' + name + '>' + es + '</option>';
             $('#lista-paises').append(html);
         });
     });
     $("#lista-paises").change(function () {
         var paisSelected = $(this).children("option:selected").val();
+        var paisName = $(this).children("option:selected").data( "pais" );
+
         State.variables.pais = paisSelected;
+
+        $.get('https://restcountries.eu/rest/v2/name/' + paisName, function (data) {
+            $.each(data, function (index, val) {
+                var name = val.name;
+                var population = val.population;
+                var deathPopulation = Math.round(((population / 1000000) / 4) * 3);
+                console.log(population);
+                console.log(deathPopulation);
+                State.variables.deathPopulation= deathPopulation;
+            });
+        });
+
     });
 });
