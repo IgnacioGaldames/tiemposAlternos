dc88baaee7da7051e83b16ca4d0543dd000dd921
Ignacio Galdames <ignacio.g@desafiolatam.com>
2020-02-05 16:59:57 -0300

    añadir selector país

--- assets/js/restcountries.js
+++ assets/js/restcountries.js
@@ -1,13 +1,20 @@
-jQuery(document).ready(function($) {
-    $.get('//restcountries.eu/rest/v2/all', function(data) {
-        $.each(data, function(index, val) {
-            var html = '<option>'+ val.translations.es + '</option>';
-            console.log(html)
+jQuery(document).ready(function ($) {
+    $.get('//restcountries.eu/rest/v2/all', function (data) {
+        $.each(data, function (index, val) {
+            var name = val.name;
+            var es = val.translations.es;
+            if (val.translations.es == null) {
+                es = name;
+            }
+            var capital = val.capital;
+            var timezones = val.timezones;
+            var population = val.population;
+            var html = '<option>' + es + '</option>';
             $('#lista-paises').append(html);
         });
     });
-    $("#lista-paises").change(function(){
+    $("#lista-paises").change(function () {
         var paisSelected = $(this).children("option:selected").val();
-        State.variables.pais =  paisSelected;
+        State.variables.pais = paisSelected;
     });
 });
