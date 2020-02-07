5257fe38dd855c0dece9a3140029dfb74c317ad2
Ignacio Galdames <ignacio.g@desafiolatam.com>
2020-02-07 18:07:25 -0300

    intentar capturar variable de label on chnage

---
+++ assets/js/label.js
@@ -0,0 +1,5 @@
+$("#textbox-playername").change(function () {
+    var textboxPlayername = $("#textbox-playername").val();
+    State.variables.playerName = textboxPlayername;
+    console.log(textboxPlayername);
+});
