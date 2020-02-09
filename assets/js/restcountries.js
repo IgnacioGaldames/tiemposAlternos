b8d340f2be4003d806cb55ddf81524cae068f527
Ignacio Galdames <caminantestelar@gmail.com>
2020-02-09 15:15:26 -0300

    sistema de personalidad mayer briggs

--- assets/js/restcountries.js
+++ assets/js/restcountries.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     $.get('//restcountries.eu/rest/v2/region/americas', function(data) {
         $.each(data, function(index, val) {
             var html = '<option>'+ val.translations.es + '</option>';
-            console.log(html)
+            //console.log(html)
             $('#lista-paises').append(html);
         });
     });
