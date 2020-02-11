69b5bbfbc5defb857aec7df2ba55dd9fc77c7ed2
Ignacio Galdames <ignacio.g@desafiolatam.com>
2020-02-11 11:56:25 -0300

    avanzar historia

--- assets/js/label.js
+++ assets/js/label.js
@@ -2,4 +2,8 @@ $("#textbox-playername").change(function () {
     var textboxPlayername = $("#textbox-playername").val();
     State.variables.playerName = textboxPlayername;
     console.log(textboxPlayername);
+    console.log('cambio');
 });
+$("input").change(function(){
+    alert("The text has been changed.");
+  });
