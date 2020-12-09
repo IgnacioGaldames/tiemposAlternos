4a1c8f2676ba7b3453dc11b4fbacd6c44de51888
Ignacio Galdames <ignacio.g@desafiolatam.com>
2020-12-09 14:43:22 -0300

    start if country selected

--- assets/js/restcountries.js
+++ assets/js/restcountries.js
@@ -1,31 +1,47 @@
 jQuery(document).ready(function ($) {
-    $.get('https://restcountries.eu/rest/v2/region/americas?fields=name;translations', function (data) {
-        $.each(data, function (index, val) {
-            var name = val.name;
-            var es = val.translations.es;
-            if (val.translations.es == null) {
-                es = name;
-            }
-            var html = '<option data-pais=' + name + '>' + es + '</option>';
-            $('#lista-paises').append(html);
-        });
-    });
-    $("#lista-paises").change(function () {
-        var paisSelected = $(this).children("option:selected").val();
-        var paisName = $(this).children("option:selected").data( "pais" );
+	$.get('https://restcountries.eu/rest/v2/region/americas?fields=name;translations', function (data) {
+		$.each(data, function (index, val) {
+			var name = val.name;
+			var es = val.translations.es;
+			if (val.translations.es == null) {
+				es = name;
+			}
+			var html = '<option data-pais=' + name + '>' + es + '</option>';
+			$('#lista-paises').append(html);
+		});
+	});
+	$("#lista-paises").change(function () {
+		var paisSelected = $(this).children("option:selected").val();
+		var paisName = $(this).children("option:selected").data("pais");
 
-        State.variables.pais = paisSelected;
+		State.variables.pais = paisSelected;
 
-        $.get('https://restcountries.eu/rest/v2/name/' + paisName, function (data) {
-            $.each(data, function (index, val) {
-                var name = val.name;
-                var population = val.population;
-                var capital = val.capital;
-                var deathPopulation = Math.round(((population / 1000000) / 4) * 3);
-                State.variables.deathPopulation= deathPopulation;
-                State.variables.capital= capital;
-            });
-        });
+		$.get('https://restcountries.eu/rest/v2/name/' + paisName, function (data) {
+			$.each(data, function (index, val) {
+				var name = val.name;
+				var population = val.population;
+				var capital = val.capital;
+				var deathPopulation = Math.round(((population / 1000000) / 4) * 3);
+				State.variables.deathPopulation = deathPopulation;
+				State.variables.capital = capital;
+			});
+		});
 
-    });
+	});
+
+	if($('#lista-paises').val()){
+		$("#juegoNuevo").attr('disabled',true);
+		$("#nuevoJuego").attr('disabled',true);
+	} else {
+		$("#juegoNuevo").attr('disabled',false);
+		$("#nuevoJuego").attr('disabled',false);
+	}
+	
+	$('#lista-paises').on('change', function()
+	{
+		$("#juegoNuevo").removeClass( "disabled btn-secondary" ).addClass( "btn-primary" );
+		$("#nuevoJuego").removeClass( "disabled btn-secondary" ).addClass( "btn-primary" );
+		$("#juegoNuevo").attr('disabled',false);
+		$("#nuevoJuego").attr('disabled',false);
+	});
 });
